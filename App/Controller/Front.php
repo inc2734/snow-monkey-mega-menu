4e79c746be0a47eab04b41cec13138e7aabfacfd
inc2734 <inc@2inc.org>
2024-12-10 16:18:25 +0900

    fix #22

--- App/Controller/Front.php
+++ App/Controller/Front.php
@@ -59,7 +59,10 @@ class Front {
 		$thumbnail_size = apply_filters( 'snow_monkey_mega_menu_thumbnail_size', 'medium', $mega_menu );
 
 		$thumbnail = '';
-		if ( 'post_type_archive' === $item->type && 'product' === $item->object ) {
+		if ( in_array( $item->type, array( 'post_type_archive', 'post_type' ), true ) && 'forum' === $item->object ) {
+			$post_type_object = get_post_type_object( $item->object );
+			$thumbnail        = Helper\Page_Header\BbPress_Archive_Page_Header::get_image( $post_type_object, $thumbnail_size );
+		} else if ( 'post_type_archive' === $item->type && 'product' === $item->object ) {
 			$post_type_object = get_post_type_object( $item->object );
 			$thumbnail        = Helper\Page_Header\WooCommerce_Archive_Page_Header::get_image( $post_type_object, $thumbnail_size );
 		} elseif ( 'taxonomy' === $item->type && in_array( $item->object, array( 'product_cat', 'product_tag' ), true ) ) {
